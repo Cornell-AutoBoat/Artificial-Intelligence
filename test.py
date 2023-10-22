87445329f49a56dbc444d32209be287ee84cf56c
Joanna Lin <jl2748@cornell.edu>
2023-10-22 16:20:08 -0400

    fixed map_to_global in utils, updated panama canal to be xy coords, added test cases for map_to_global

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import unittest
 from src.buoys import Buoy
 from src.control_tasks import utils
-
+from src import SFR
+import numpy as np
 
 class TestSeen(unittest.TestCase):
     def test_one_object_true(self):
@@ -48,7 +49,150 @@ class TestSeen(unittest.TestCase):
         self.assertEqual(utils.seen(o1, s), True, "Couldn't identify that buoys were the same")
 
 class TestMapToGlobal(unittest.TestCase):
-    pass
+    def test_zero_zero_origin_no_heading(self):
+        x = 0.0
+        y = 0.0
+        SFR.tx = 0.0
+        SFR.ty = 0.0
+        SFR.tz = 0.0
+        SFR.heading = 0.0
+        self.assertEqual(utils.map_to_global(x, y), [0.0, 0.0], "Wrong global coordinates for local (0.0, 0.0) at origin")
+
+    def test_three_three_origin_no_heading(self):
+        x = 3.0
+        y = 3.0
+        SFR.tx = 0.0
+        SFR.ty = 0.0
+        SFR.tz = 0.0
+        SFR.heading = 0.0
+        self.assertEqual(utils.map_to_global(x, y), [3.0, -3.0], "Wrong global coordinates for local (3.0, 3.0) at origin")
+
+    def test_three_three_SFR_no_heading(self):
+        x = 3.0
+        y = 3.0
+        SFR.tx = 3.0
+        SFR.ty = 3.0
+        SFR.tz = 0.0
+        SFR.heading = 0.0
+        self.assertEqual(utils.map_to_global(x, y), [6.0, 0.0], "Wrong global coordinates for local (3.0, 3.0) not at origin")
+      
+    def test_two_two_SFR_no_heading(self):
+        x = 2.0
+        y = 2.0
+        SFR.tx = 4.0
+        SFR.ty = 4.0
+        SFR.tz = 0.0
+        SFR.heading = 0.0
+        self.assertEqual(utils.map_to_global(x, y), [6.0, 2.0], "Wrong global coordinates for local (2.0, 2.0) not at origin")
+
+    def test_one_two_SFR_no_heading(self):
+        x = 1.0
+        y = 2.0
+        SFR.tx = 4.0
+        SFR.ty = 2.0
+        SFR.tz = 0.0
+        SFR.heading = 0.0
+        self.assertEqual(utils.map_to_global(x, y), [6.0, 1.0], "Wrong global coordinates for local (1.0, 2.0) not at origin")
+
+    def test_zero_zero_origin_zero_theta(self):
+        x = 0.0
+        y = 0.0
+        SFR.tx = 0.0
+        SFR.ty = 0.0
+        SFR.tz = 0.0
+        SFR.heading = np.pi/2
+        self.assertEqual(utils.map_to_global(x, y), [0.0, 0.0], "Wrong global coordinates for local (0.0, 0.0) at origin with zero theta")
+
+    def test_three_three_origin_zero_theta(self):
+        x = 3.0
+        y = 3.0
+        SFR.tx = 0.0
+        SFR.ty = 0.0
+        SFR.tz = 0.0
+        SFR.heading = np.pi/2
+        self.assertEqual(utils.map_to_global(x, y), [3.0, 3.0], "Wrong global coordinates for local (3.0, 3.0) at origin with zero theta")
+
+    def test_three_three_SFR_zero_theta(self):
+        x = 3.0
+        y = 3.0
+        SFR.tx = 3.0
+        SFR.ty = 3.0
+        SFR.tz = 0.0
+        SFR.heading = np.pi/2
+        self.assertEqual(utils.map_to_global(x, y), [6.0, 6.0], "Wrong global coordinates for local (3.0, 3.0) not at origin with zero theta")
+
+    def test_one_two_SFR_positive_theta(self):
+        x = 1.0
+        y = 2.0
+        SFR.tx = 4.0
+        SFR.ty = 2.0
+        SFR.tz = 0.0
+        SFR.heading = np.pi
+        self.assertEqual(utils.map_to_global(x, y), [2.0, 3.0], "Wrong global coordinates for local (1.0, 2.0) not at origin with positive theta")
+
+    def test_zero_zero_origin_negative_theta(self):
+        x = 0.0
+        y = 0.0
+        SFR.tx = 0.0
+        SFR.ty = 0.0
+        SFR.tz = 0.0
+        SFR.heading = (-3 * np.pi)/4
+        self.assertEqual(utils.map_to_global(x, y), [0.0, 0.0], "Wrong global coordinates for local (0.0, 0.0) at origin with negative theta")
+    
+    def test_two_two_SFR_negative_theta(self):
+        x = 2.0
+        y = 2.0
+        SFR.tx = 4.0
+        SFR.ty = 4.0
+        SFR.tz = 0.0
+        SFR.heading = (-3 * np.pi)/4
+        self.assertEqual(utils.map_to_global(x, y), [1.1715728752538102, 4.0], "Wrong global coordinates for local (2.0, 2.0) not at origin with negative theta")
+
+    def test_one_two_SFR_negative_theta(self):
+        x = 1.0
+        y = 2.0
+        SFR.tx = 4.0
+        SFR.ty = 2.0
+        SFR.tz = 0.0
+        SFR.heading = (-3 * np.pi)/4
+        self.assertEqual(utils.map_to_global(x, y), [1.8786796564403572, 1.2928932188134525], "Wrong global coordinates for local (1.0, 2.0) not at origin with negative theta")
+
+    def test_neg_five_two_SFR_negative_theta(self):
+        x = -5.0
+        y = 2.0
+        SFR.tx = 4.0
+        SFR.ty = -1.0
+        SFR.tz = 0.0
+        SFR.heading = (-3 * np.pi)/4
+        self.assertEqual(utils.map_to_global(x, y), [6.121320343559642, -5.949747468305833], "Wrong global coordinates for local (-5.0, 2.0) not at origin with negative theta")
+
+    def test_five_neg_two_SFR_negative_theta(self):
+        x = 5.0
+        y = -2.0
+        SFR.tx = 4.0
+        SFR.ty = -1.0
+        SFR.tz = 0.0
+        SFR.heading = (-3 * np.pi)/4
+        self.assertEqual(utils.map_to_global(x, y), [1.878679656440358, 3.9497474683058327], "Wrong global coordinates for local (5.0, -2.0) not at origin with negative theta")
+
+    def test_neg_five_neg_two_SFR_negative_theta(self):
+        x = -5.0
+        y = -2.0
+        SFR.tx = 4.0
+        SFR.ty = -1.0
+        SFR.tz = 0.0
+        SFR.heading = (-3 * np.pi)/4
+        self.assertEqual(utils.map_to_global(x, y), [8.949747468305834, -3.121320343559643], "Wrong global coordinates for local (5.0, -2.0) not at origin with negative theta")
+
+    def test_five_neg_two_neg_SFR_negative_theta(self):
+        x = -5.0
+        y = -2.0
+        SFR.tx = -4.0
+        SFR.ty = -1.0
+        SFR.tz = 0.0
+        SFR.heading = (-3 * np.pi)/4
+        self.assertEqual(utils.map_to_global(x, y), [0.9497474683058327, -3.121320343559643], "Wrong global coordinates for local (5.0, -2.0) not at origin with negative theta")
+
 
 class TestGetMidpoint(unittest.TestCase):
     pass
